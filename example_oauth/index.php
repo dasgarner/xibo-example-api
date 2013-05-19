3c0cc75abee75708328bf8cb3d2b159aab6cbe71
Dan Garner <dan@xibo.org.uk>
2013-05-19 23:40:10 +0100

    [server] Fixed API access and ensured up to date example_oauth

--- example_oauth/index.php
+++ example_oauth/index.php
@@ -5,7 +5,14 @@ require_once('oauth-php/library/OAuthRequestLogger.php');
 
 DEFINE('OAUTH_LOG_REQUEST', true);
 
-OAuthStore::instance('MySQL', array('server' => 'localhost', 'username' => 'root', 'password' => 'danjim101', 'database' => 'oauthconsumer'));
+$connection = array('server' => 'localhost', 
+    'username' => 'root', 
+    'password' => '', 
+    'database' => 'oauth_consumer');
+
+OAuthStore::instance('MySQL', $connection);
+
+DEFINE('SERVER_BASE', 'http://localhost/xibo/1.5/server-151-api/server/');
 
 switch($_GET['action'])
 {
@@ -34,22 +41,25 @@ function AddServerToOAuth()
 
     // The server description
     $server = array(
-        'consumer_key' => '31662344be43f3818473abb099c8b8a204b81544d',
-        'consumer_secret' => '344095a4a78fb59933a79464fb103325',
-        'server_uri' => 'http://localhost/Series%201.1/111-server/server/services.php?service=rest',
+        'consumer_key' => 'e5adc06021aa90157114862e5a22287d0519918d2',
+        'consumer_secret' => '9411a8a5d9a8fc63c105cb3119a89395',
         'signature_methods' => array('HMAC-SHA1', 'PLAINTEXT'),
-        'request_token_uri' => 'http://localhost/Series%201.1/111-server/server/services.php?service=oauth&method=request_token',
-        'authorize_uri' => 'http://localhost/Series%201.1/111-server/server/index.php?p=oauth&q=authorize',
-        'access_token_uri' => 'http://localhost/Series%201.1/111-server/server/index.php?p=oauth&q=access_token'
+        'server_uri' => SERVER_BASE . 'services.php?service=rest',
+        'request_token_uri' => SERVER_BASE . 'services.php?service=oauth&method=request_token',
+        'authorize_uri' => SERVER_BASE . 'index.php?p=oauth&q=authorize',
+        'access_token_uri' => SERVER_BASE . 'services.php?service=oauth&method=access_token'
     );
 
     // Save the server in the the OAuthStore
     $consumer_key = $store->updateServer($server, $user_id);
+
+    echo('Server Added');
 }
 
 function ObtainAccessToAServer()
 {
-    $consumer_key = '31662344be43f3818473abb099c8b8a204b81544d';
+    // You request servers using their consumer key
+    $consumer_key = 'e5adc06021aa90157114862e5a22287d0519918d2';
     $user_id = 1;
 
     // Obtain a request token from the server
@@ -64,7 +74,7 @@ function ObtainAccessToAServer()
     }
 
     // Callback to our (consumer) site, will be called when the user finished the authorization at the server
-    $callback_uri = 'http://localhost/consumer/index.php?action=Exchange&consumer_key='.rawurlencode($consumer_key).'&usr_id='.intval($user_id);
+    $callback_uri = 'http://localhost/xibo/1.5/server-151-api/example_oauth/?action=Exchange&consumer_key='.rawurlencode($consumer_key).'&usr_id='.intval($user_id);
 
     // Now redirect to the autorization uri and get us authorized
     if (!empty($token['authorize_uri']))
@@ -117,14 +127,14 @@ function MakeSignedRequest()
 {
     // The request uri being called.
     $user_id = 1;
-    $request_uri = 'http://localhost/Series%201.1/111-server/server/services.php';
+    $request_uri = SERVER_BASE . 'services.php';
 
     // Parameters, appended to the request depending on the request method.
     // Will become the POST body or the GET query string.
     $params = array(
                'service' => 'rest',
                'method' => 'version',
-               'response' => 'json'
+               'response' => 'xml'
          );
 
     // Obtain a request object for the request we want to make
@@ -134,6 +144,6 @@ function MakeSignedRequest()
     $result = $req->doRequest($user_id);
 
     // $result is an array of the form: array ('code'=>int, 'headers'=>array(), 'body'=>string)
-    var_dump($result['body']);
+    var_dump($result);
 }
 ?>
